b25352e012fb36cbe8847324e67d678d76441cc3
youranreus <youranreus@qq.com>
2022-05-09 16:20:32 +0800

    feat: 注册方法添加字段约束

--- service/UserService.js
+++ service/UserService.js
@@ -7,19 +7,28 @@ const User = require("../models/User");
 const {Result} = require("../utils");
 const {isDev} = require("../config");
 
-
+/**
+ * 注册
+ * @param data
+ * @returns {Promise<Result>}
+ */
 async function Register(data) {
     try {
-        const u = await User.create(data)
+        // 这里需要添加fields，避免传入的data中包含了非法字段
+        const u = await User.create(data, {fields: ['name', 'nickname', 'email', 'password', 's_id']})
         return new Result('success', 0, u.toJSON())
     } catch (e) {
-        return new Result('error: ' + e.toString()
+        return new Result(e.toString()
             .replaceAll('SequelizeValidationError: ', '')
             .replaceAll('Validation error: ', '')
             .replaceAll('\n', ' '), 1)
     }
 }
 
+/**
+ * 检查状态
+ * @returns {Promise<Result>}
+ */
 async function checkStatus() {
     try {
         await authenticate()
