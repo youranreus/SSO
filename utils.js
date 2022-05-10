0b39d1514e850275952dbf1a30748cc0f6b97a5a
youranreus <youranreus@qq.com>
2022-05-10 19:46:17 +0800

    feat: 工具类添加的md5加密函数

--- utils.js
+++ utils.js
@@ -2,6 +2,7 @@
  * @author 季悠然
  * @date 2022-05-07
  */
+const CRYPTO = require('crypto')
 
 /**
  * 是否已定义
@@ -17,6 +18,26 @@ const isDef = v => v !== undefined && v !== null
  */
 const checkParams = (keys, obj) => keys.every(k => isDef(obj[k]))
 
+/**
+ * 加密
+ * @param v
+ * @returns {string}
+ */
+const md5Crypto = v => {
+    const md5 = CRYPTO.createHash('md5')
+
+    // 每次分段加密的字符串最大长度
+    while (v) {
+        const tempV = v.slice(0, 5)
+        v = v.slice(5)
+        md5.update(`${tempV} - `)
+    }
+    return md5.digest('hex')
+}
+
+/**
+ * 结果类
+ */
 class Result {
     constructor(msg = 'ok', code = 0, data = {}) {
         this.msg = msg
@@ -28,5 +49,6 @@ class Result {
 module.exports = {
     isDef,
     checkParams,
-    Result
+    Result,
+    md5Crypto
 }
