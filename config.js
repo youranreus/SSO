a01b09b9bc866d70c55fb635ff13ae8643a8c2c9
youranreus <youranreus@qq.com>
2022-05-13 15:02:07 +0800

    update: .env.example

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const DAY = 24 * HOUR
 const DBConfig = [
     process.env.DB_NAME,
     process.env.DB_USERNAME,
-    process.env.DB_PWD,
+    process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
         dialect: process.env.DB_TYPE,
