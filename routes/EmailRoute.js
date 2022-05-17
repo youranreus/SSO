8304dda0cab7c3e8e787241583a5daf1ac0bbd87
youranreus <youranreus@qq.com>
2022-05-17 22:28:59 +0800

    feat: 将获取邮件验证码的参数传递改为query

--- routes/EmailRoute.js
+++ routes/EmailRoute.js
@@ -3,7 +3,7 @@
  * @date 2022-05-07
  */
 const express = require('express')
-const {checkParams, Result} = require("../utils")
+const {Result} = require("../utils")
 const EmailService = require('../service/EmailService')
 const router = express.Router()
 
@@ -11,13 +11,13 @@ const router = express.Router()
  * 获取验证码
  */
 router.get('/captcha', async (req, res) => {
-    if (!checkParams(['email'], req.body))
+    if (!req.query.email || req.query.type === '')
         res.status(400).json(new Result('参数缺失'))
     if(!req.query.type || req.query.type === 'reg' || req.query.type === '') {
-        const _res = await EmailService.sendReg(req.body.email)
+        const _res = await EmailService.sendReg(req.query.email)
         res.status(_res.code).json(_res)
     } else if (req.query.type === 'reset') {
-        const _res = await EmailService.sendReset(req.body.email)
+        const _res = await EmailService.sendReset(req.query.email)
         res.status(_res.code).json(_res)
     }
 })
