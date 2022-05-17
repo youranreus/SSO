ae6e08690f5f5822341adb4a9b3d24db5c3cfff1
youranreus <youranreus@qq.com>
2022-05-17 21:03:19 +0800

    feat: 配置跨域问题

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const EmailRoute = require('./routes/EmailRoute')
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use((req, res, next) => {
-    res.result = {
-        msg: "",
-        code: 0,
-        data: {}
-    }
-
-    next()
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin","*");
+    //允许的header类型
+    res.header("Access-Control-Allow-Headers","content-type");
+    //跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() === 'options')
+        res.send(200);  //让options尝试请求快速结束
+    else
+        next()
 })
 
 app.use('/api/user', UserRoute)
