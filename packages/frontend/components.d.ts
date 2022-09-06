9f6b58acddc0f6e2f01b5fee8eb33abef356b5af
youranreus <youranreus@qq.com>
2022-09-06 21:58:20 +0800

    chore: unplugin-vue-components自动更新

--- packages/frontend/components.d.ts
+++ packages/frontend/components.d.ts
@@ -5,6 +5,12 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AAvatar: typeof import('@arco-design/web-vue')['Avatar']
+    AButton: typeof import('@arco-design/web-vue')['Button']
+    ADescriptions: typeof import('@arco-design/web-vue')['Descriptions']
+    ADivider: typeof import('@arco-design/web-vue')['Divider']
+    ASkeleton: typeof import('@arco-design/web-vue')['Skeleton']
+    ASkeletonLine: typeof import('@arco-design/web-vue')['SkeletonLine']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
